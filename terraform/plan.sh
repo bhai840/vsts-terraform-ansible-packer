acfae7504a0de6af6653b9758f90dc8dcbc3028b
bhai840 <bhai840@rediffmail.com>
2020-03-24 15:31:21 +1100

    updated plan

--- terraform/plan.sh
+++ terraform/plan.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-echo "************* execute terraform init"
+#echo "************* execute terraform init"
 ## execute terrafotm build and sendout to packer-build-output
 
-terraform plan 
+#terraform plan 
+
+ls -la
+echo "************* execute terraform plan -var manageddiskname=$6"
+
+## execute terrafotm build and sendout to packer-build-output
+export ARM_CLIENT_ID=$1
+export ARM_CLIENT_SECRET=$2
+export ARM_SUBSCRIPTION_ID=$3
+export ARM_TENANT_ID=$4
+export ARM_ACCESS_KEY=$5
+
+terraform plan -var "manageddiskname=$6"
