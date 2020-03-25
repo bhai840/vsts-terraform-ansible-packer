e0b9abb3c833295aeecad6d386a98e2695b6bfca
bhai840 <bhai840@rediffmail.com>
2020-03-25 22:07:53 +1100

    added escape character

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -141,7 +141,7 @@ resource "azurerm_storage_account" "demo_storage_account" {
 
 # Points to Packer build image 
 data "azurerm_image" "image" {
-  name                =  "demoPackerImage-2020-03-24_04_40_17" #var.manageddiskname
+  name                =  var.manageddiskname #"demoPackerImage-2020-03-24_04_40_17" #var.manageddiskname
   resource_group_name =  var.manageddiskname_rg
 }
 output "image_id" {
@@ -209,5 +209,5 @@ output "vm_ip" {
 }
 
 output "vm_dns" {
-  value = http://azurerm_public_ip.demo_public_ip.domain_name_label.SouthCentralUS.cloudapp.azure.com
+  value = http\://azurerm_public_ip.demo_public_ip.domain_name_label.SouthCentralUS.cloudapp.azure.com
 }
