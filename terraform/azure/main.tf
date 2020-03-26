1504a0bdfec853fec461eff2d2675396e3f5c49a
bhai840 <bhai840@rediffmail.com>
2020-03-26 23:51:05 +1100

    Update and rename main.tf.old to main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -141,8 +141,9 @@ resource "azurerm_storage_account" "demo_storage_account" {
 
 # Points to Packer build image 
 data "azurerm_image" "image" {
-  name_regex          =  "demoPackerImage-2020-03-24_04_40_17" # var.manageddiskname #demoPackerImage-2020-03-24_04_40_17" #var.manageddiskname
-  resource_group_name =  "manageddiskname"
+  name_regex          =  "[[:alnum:]]" 
+  sort_descending     = true
+  resource_group_name =  var.manageddiskname-rg
 }
 output "image_id" {
   value = data.azurerm_image.image.id
