8e24856098c78fc7e0fe520e8c87ebf980870f31
bhai840 <bhai840@rediffmail.com>
2020-03-26 17:17:59 +1100

    Update variables.tf

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -16,11 +16,4 @@ variable "baked_image_url" {
 
 variable "image_id" {
   default = ""
-  #demoPackerImage-formatdate("YYYY-MM-DD_hh_mm_ss", "2018-01-02T23:12:01")
-  #"demoPackerImage-2020-03-24_04_40_17"
-  #validation {
-    # regex(...) fails if it cannot find a match
-   # condition    = can (regex(demoPackerImage-formatdate("YYYY-MM-DD_hh_mm_ss", "2018-01-02T23:12:01"), var.image_id))
-    
-    #}
 }
